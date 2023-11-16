f2516fe827be38245b8cea40dbcc182f2ac9dcaa
Dawid Laskowski <117516707+dawlas@users.noreply.github.com>
2023-11-16 11:48:56 +0100

    first commit

---
+++ themes/netloom/index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Main template file.
+ * 
+ * @package Netloom
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wordpress theme</title>
+</head>
+<body>
+    
+</body>
+</html>
